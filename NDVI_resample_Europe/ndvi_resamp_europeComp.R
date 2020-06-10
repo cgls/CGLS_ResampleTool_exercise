97869b1efe9514c87469dec63b68287d83ed3cc8
Xavier Rotllan-Puig <xavi.rotllan.puig@gmail.com>
2020-06-10 15:52:12 +0200

    more statistics; also QGIS_orig1km and QGIS_R

--- NDVI_resample_Europe/ndvi_resamp_europeComp.R
+++ NDVI_resample_Europe/ndvi_resamp_europeComp.R
@@ -41,9 +41,6 @@ nc_file300m <- paste0(path2data, "c_gls_NDVI300_PROD-DESC_202005010000_GLOBE_PRO
 qgis_resamp_europe_avrge <- paste0(path2data, "europa1000_aver.tif")
 ndvi_1km_orig <- paste0(path2data, "/ndvi_v2_1km_c_gls_NDVI_202005010000_GLOBE_PROBAV_V2.2.1.nc")
 
-# Do the images need to be cropped??
-subsetting <- "yes"  
-
 
 
 ## Reading in data QGIS resampled average ####
@@ -173,15 +170,33 @@ r300m_resampled1km_Aggr <- aggregate(ndvi300m_rstr,
                                      fact = 3, # from 333m to 1km  
                                      fun = aggr_method, 
                                      na.rm = TRUE, 
-                                     filename = 'r300m_resampled1km_Aggr.tif')
+                                     filename = 'r300m_resampled1km_Aggr.tif',
+                                     overwrite = TRUE)
 Sys.time() - t0
 r300m_resampled1km_Aggr
 
-
+# plotting resampled map
 jpeg(paste0(path2save, "/r300m_resampled1km_Aggr.jpg"))
 plot(r300m_resampled1km_Aggr, main = "r300m_resampled1km_Aggr")
 dev.off()
 
+
+# plotting original 1km and 300m
+jpeg(paste0(path2save, "/ndvi1km_300m_Eur.jpg"),
+     width = 22, height = 14, units = "cm", res = 300)
+par(mfrow = c(1, 2), mar = c(4, 4, 4, 5))
+plot(ndvi1km_rstr, main = "NDVI 1km")
+plot(ndvi300m_rstr, main = "NDVI 333") 
+dev.off()
+
+
+# plotting original-1km + resampled-1km
+jpeg(paste0(path2save, "/ndvi1km_1kmResampled_RAggr.jpg"),
+     width = 22, height = 14, units = "cm", res = 300)
+par(mfrow = c(1, 2), mar = c(4, 4, 4, 5))
+plot(ndvi1km_rstr, main = "NDVI 1km (original)")
+plot(r300m_resampled1km_Aggr, main = "NDVI 1km (resampled)") 
+dev.off()
 
 
 
@@ -196,51 +211,187 @@ r300m_resampled1km_Bilinear <- resample(ndvi300m_rstr, ndvi1km_rstr,
 
 
 
-## Comparison 'original-1km' with '300m-resampled-1km-Aggr' ####
+## Comparison 'original-1km' with '300m-resampled-1km-R_Aggr' ####
+comp_results <- as.data.frame(matrix(ncol = 4))  #to store results
+names(comp_results) <- c("objects", 
+                         "Pearson's r", "Root Mean Square Error", "Mean Absolute Error")
+comp_results[1, 1] <- "orig-1km__resampl-1km-R-Aggreg"
+
 rsmpl_df <- data.frame(getValues(ndvi1km_rstr), getValues(r300m_resampled1km_Aggr))
 
 sum(complete.cases(rsmpl_df))
 rsmpl_df <- rsmpl_df[complete.cases(rsmpl_df), 1:2]
 
+# Pearson's correlation coefficient
 rsmpl_df_pearson <- cor(rsmpl_df, method = "pearson")[2, 1]
 rsmpl_df_pearson
+rsmpl_df_pearson^2  # if we fit a linear regression (see below), this is R^2 (R squared)
+comp_results[1, 2] <- rsmpl_df_pearson
 
-perc_subsample <- 1
+# Plotting correlation (scatterplot)
+perc_subsample <- 1   # percentage of points for plotting
 num_subsample <- round((nrow(rsmpl_df) * perc_subsample / 100), 0)
 rsmpl_df_subsample <- rsmpl_df[sample(nrow(rsmpl_df), num_subsample), ]
 
 jpeg(paste0(path2save, "/resample_correlation_RAggr.jpg"))
 xyplot(rsmpl_df_subsample$getValues.r300m_resampled1km_Aggr. ~ rsmpl_df_subsample$getValues.ndvi1km_rstr., 
-       type = c("p"),
+       type = c("p", "r"),
+       col.line = "red",
+       xlab = "1km original NDVI product",
+       ylab = "1km resampled NDVI image (R)",
        main = paste0("Pearson's r = ", as.character(round(rsmpl_df_pearson, 4))),
-       sub = paste0("Plotting a random subsample of ", num_subsample, " (", perc_subsample, "%) points"))
+       sub = paste0("Plotting a random subsample of ", num_subsample, " (", perc_subsample, "%) points")
+       )
 dev.off()
 
 
+# Calculating differences (errors)
+head(rsmpl_df)
+rsmpl_df$diff <- abs(rsmpl_df$getValues.ndvi1km_rstr. - rsmpl_df$getValues.r300m_resampled1km_Aggr.)
+rsmpl_df$diff1 <- abs(round(rsmpl_df$getValues.ndvi1km_rstr., 1) - round(rsmpl_df$getValues.r300m_resampled1km_Aggr., 1))
+rsmpl_df$diff3 <- abs(round(rsmpl_df$getValues.ndvi1km_rstr., 3) - round(rsmpl_df$getValues.r300m_resampled1km_Aggr., 3))
 
-jpeg(paste0(path2save, "/ndvi1km_300m_Eur.jpg"),
-     width = 22, height = 14, units = "cm", res = 300)
-par(mfrow = c(1, 2), mar = c(4, 4, 4, 5))
-plot(ndvi1km_rstr, main = "NDVI 1km")
-plot(ndvi300m_rstr, main = "NDVI 333") 
-dev.off()
+summary(rsmpl_df$diff)
+summary(rsmpl_df$diff1)
+quantile(rsmpl_df$diff1, seq(0, 1, 0.1))
+summary(rsmpl_df$diff3) # not substantial differences with 'rsmpl_df$diff'
+
+1/250 # 0.004 is the amount of physical or real value (for NDVI, -0,08:0.92) 
+      # for each digital number (0:250), so at least 3 decimals should be included
+#
+
+# Root Mean Square Error (RMSE; the lower, the better)
+# In GIS, the RMSD is one measure used to assess the accuracy of spatial analysis and remote sensing.
+rmse <- sqrt(mean((rsmpl_df$diff)^2)) 
+comp_results[1, 3] <- rmse
+
+# Mean Absolute Error (MAE; the lower, the better)
+mae <- mean(rsmpl_df$diff)
+comp_results[1, 4] <- mae
 
 
-
-jpeg(paste0(path2save, "/ndvi1km_1kmResampled_RAggr.jpg"),
-     width = 22, height = 14, units = "cm", res = 300)
-par(mfrow = c(1, 2), mar = c(4, 4, 4, 5))
-plot(ndvi1km_rstr, main = "NDVI 1km (original)")
-plot(r300m_resampled1km_Aggr, main = "NDVI 1km (resampled)") 
-dev.off()
+# Bivariate Linear Regression
+lm_obj <- lm(rsmpl_df$getValues.ndvi1km_rstr. ~ rsmpl_df$getValues.r300m_resampled1km_Aggr.)
+summary(lm_obj)
+lm_obj_summary <- summary(lm_obj)
+round(lm_obj_summary$r.squared, 10) == round(rsmpl_df_pearson^2, 10)
 
 
-
-
-
-stuff2save <- c("cuttoff_NA_err", "rsmpl_df_pearson", "badResamplingHighProp", "badResamplingLowProp")
+# Saving stuff for the report
+stuff2save <- c("comp_results")
 save(list = stuff2save, file = paste0(path2save, "/ResampleResults4Report.RData"))
 
+
+
+
+## Comparison 'original-1km' with '300m-resampled-1km-QGIS_Aggr' ####
+comp_results[2, 1] <- "orig-1km__resampl-1km-QGIS-Aggreg"
+
+rsmpl_df <- data.frame(getValues(ndvi1km_rstr), getValues(qgis_resamp_europe_avrge))
+rsmpl_df <- rsmpl_df[complete.cases(rsmpl_df), 1:2]
+
+# Pearson's correlation coefficient
+rsmpl_df_pearson <- cor(rsmpl_df, method = "pearson")[2, 1]
+comp_results[2, 2] <- rsmpl_df_pearson
+
+# Plotting correlation (scatterplot)
+#perc_subsample <- 1   # percentage of points for plotting
+num_subsample <- round((nrow(rsmpl_df) * perc_subsample / 100), 0)
+rsmpl_df_subsample <- rsmpl_df[sample(nrow(rsmpl_df), num_subsample), ]
+
+jpeg(paste0(path2save, "/resample_correlation_QGISAggr.jpg"))
+xyplot(rsmpl_df_subsample$getValues.qgis_resamp_europe_avrge. ~ rsmpl_df_subsample$getValues.ndvi1km_rstr., 
+       type = c("p", "r"),
+       col.line = "red",
+       xlab = "1km original NDVI product",
+       ylab = "1km resampled NDVI image (QGIS)",
+       main = paste0("Pearson's r = ", as.character(round(rsmpl_df_pearson, 4))),
+       sub = paste0("Plotting a random subsample of ", num_subsample, " (", perc_subsample, "%) points")
+)
+dev.off()
+
+
+# Calculating differences (errors)
+head(rsmpl_df)
+rsmpl_df$diff <- abs(rsmpl_df$getValues.ndvi1km_rstr. - rsmpl_df$getValues.qgis_resamp_europe_avrge.)
+rsmpl_df$diff1 <- abs(round(rsmpl_df$getValues.ndvi1km_rstr., 1) - round(rsmpl_df$getValues.qgis_resamp_europe_avrge., 1))
+
+summary(rsmpl_df$diff)
+summary(rsmpl_df$diff1)
+quantile(rsmpl_df$diff1, seq(0, 1, 0.1))
+
+1/250 # 0.004 is the amount of physical or real value (for NDVI, -0,08:0.92) 
+# for each digital number (0:250), so at least 3 decimals should be included
+#
+
+# Root Mean Square Error (RMSE; the lower, the better)
+# In GIS, the RMSD is one measure used to assess the accuracy of spatial analysis and remote sensing.
+rmse <- sqrt(mean((rsmpl_df$diff)^2)) 
+comp_results[2, 3] <- rmse
+
+# Mean Absolute Error (MAE; the lower, the better)
+mae <- mean(rsmpl_df$diff)
+comp_results[2, 4] <- mae
+
+
+
+
+## Comparison '300m-resampled-1km-R_Aggr' with '300m-resampled-1km-QGIS_Aggr' ####
+comp_results[3, 1] <- "resampl-1km-R-Aggreg__resampl-1km-QGIS-Aggreg"
+
+rsmpl_df <- data.frame(getValues(r300m_resampled1km_Aggr), getValues(qgis_resamp_europe_avrge))
+rsmpl_df <- rsmpl_df[complete.cases(rsmpl_df), 1:2]
+
+# Pearson's correlation coefficient
+rsmpl_df_pearson <- cor(rsmpl_df, method = "pearson")[2, 1]
+comp_results[2, 2] <- rsmpl_df_pearson
+
+# Plotting correlation (scatterplot)
+#perc_subsample <- 1   # percentage of points for plotting
+num_subsample <- round((nrow(rsmpl_df) * perc_subsample / 100), 0)
+rsmpl_df_subsample <- rsmpl_df[sample(nrow(rsmpl_df), num_subsample), ]
+
+jpeg(paste0(path2save, "/resample_correlation_R_QGIS_Aggr.jpg"))
+xyplot(rsmpl_df_subsample$getValues.qgis_resamp_europe_avrge. ~ rsmpl_df_subsample$getValues.r300m_resampled1km_Aggr., 
+       type = c("p", "r"),
+       col.line = "red",
+       xlab = "1km resampled NDVI image (R)",
+       ylab = "1km resampled NDVI image (QGIS)",
+       main = paste0("Pearson's r = ", as.character(round(rsmpl_df_pearson, 4))),
+       sub = paste0("Plotting a random subsample of ", num_subsample, " (", perc_subsample, "%) points")
+)
+dev.off()
+
+
+# Calculating differences (errors)
+head(rsmpl_df)
+rsmpl_df$diff <- abs(rsmpl_df$getValues.r300m_resampled1km_Aggr. - rsmpl_df$getValues.qgis_resamp_europe_avrge.)
+rsmpl_df$diff1 <- abs(round(rsmpl_df$getValues.r300m_resampled1km_Aggr., 1) - round(rsmpl_df$getValues.qgis_resamp_europe_avrge., 1))
+
+summary(rsmpl_df$diff)
+summary(rsmpl_df$diff1)
+quantile(rsmpl_df$diff1, seq(0, 1, 0.1))
+
+1/250 # 0.004 is the amount of physical or real value (for NDVI, -0,08:0.92) 
+# for each digital number (0:250), so at least 3 decimals should be included
+#
+
+# Root Mean Square Error (RMSE; the lower, the better)
+# In GIS, the RMSD is one measure used to assess the accuracy of spatial analysis and remote sensing.
+rmse <- sqrt(mean((rsmpl_df$diff)^2)) 
+comp_results[3, 3] <- rmse
+
+# Mean Absolute Error (MAE; the lower, the better)
+mae <- mean(rsmpl_df$diff)
+comp_results[3, 4] <- mae
+
+
+
+
+
+# Saving stuff for the report
+stuff2save <- c("comp_results")
+save(list = stuff2save, file = paste0(path2save, "/ResampleResults4Report.RData"))
 
 
 
